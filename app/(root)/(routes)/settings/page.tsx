6d5b49f39b1dbaf069beb80b2e47e8bf4aac8845
benixmaxedis <benwales@bigpond.com>
2023-10-10 13:44:13 +1000

    renamed checksubscription

--- app/(root)/(routes)/settings/page.tsx
+++ app/(root)/(routes)/settings/page.tsx
@@ -1,8 +1,8 @@
 import { SubscriptionButton } from '@/components/subscription-button';
-import { checkSubcription } from '@/lib/subscription';
+import { checkSubscription } from '@/lib/subscription';
 
 const SettingsPage = async () => {
-  const isPro = await checkSubcription();
+  const isPro = await checkSubscription();
   return (
     <div className="h-full p-4 space-y-2">
       <h3 className="text-lg font-medium">Settings</h3>
